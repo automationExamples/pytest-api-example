584a944cc42a5c3b383abe8b884523004519368f
seshasai33 <seshasai33@gmail.com>
2024-01-21 09:28:02 -0600

    Added Todo tasks for tests as required

--- test_pet.py
+++ test_pet.py
@@ -2,6 +2,7 @@ from jsonschema import validate
 import pytest
 import schemas
 import api_helpers
+import json
 from hamcrest import assert_that, contains_string, is_
 
 '''
@@ -26,21 +27,35 @@ TODO: Finish this test by...
 3) Validate the 'status' property in the response is equal to the expected status
 4) Validate the schema for each object in the response
 '''
-@pytest.mark.parametrize("status", [("available")])
+@pytest.mark.parametrize("status", [("pending"),("available")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
         "status": status
     }
-
+    expected_status = status
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+    assert response.status_code == 200
+    response_data = response.json()
+    for pet_info in response_data:
+        actual_status = pet_info.get('status')
+        assert actual_status == expected_status, f"Expected status: {expected_status}, Actual status: {actual_status}"
+   
+    # Validate the schema for each object in the response
+    for pet_info in response_data:
+         validate(instance=pet_info, schema=schemas.pet)
+
 
 '''
 TODO: Finish this test by...
 1) Testing and validating the appropriate 404 response for /pets/{pet_id}
 2) Parameterizing the test for any edge cases
 '''
-def test_get_by_id_404():
+@pytest.mark.parametrize("ids", [("10050002"),("*"),("-1000001")])
+def test_get_by_id_404(ids):
     # TODO...
+    test_endpoint = "/pets/" + ids
+    response = api_helpers.get_api_data(test_endpoint)
+    assert response.status_code == 404
+
     pass
