f158e6ee686d83457d3ef1325402e0f8269eb0f8
Sindhu1302 <157327072+Sindhu1302@users.noreply.github.com>
2024-01-21 19:05:25 -0500

    Update test_store.py

--- test_store.py
+++ test_store.py
@@ -12,5 +12,21 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+@pytest.mark.parametrize("order_id", ["1"])
+def test_patch_order_by_id(order_id):
+        
+    patch_payload = {
+        "status": "pending",
+        "complete": True
+    }
+
+    response = api_helpers.patch_api_data(f"/store/order/{order_id}", patch_payload)
+
+    try:
+        assert response.status_code == 200
+
+        assert_that(response.json().get("message"), is_("Order and pet status updated successfully"))
+
+    except AssertionError as e:
+        print(f"Test failed: {e}")
+        raise  
