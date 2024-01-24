42fccaf7075d8b46bf2b09cee4d09ca9fe3f9f04
Yasaswini Kopparthi <yasaswini.kopparthi@gmail.com>
2024-01-24 11:31:46 -0600

    code changes as requested

--- test_pet.py
+++ test_pet.py
@@ -2,13 +2,10 @@ from jsonschema import validate
 import pytest
 import schemas
 import api_helpers
+import json
 from hamcrest import assert_that, contains_string, is_
 
-'''
-TODO: Finish this test by...
-1) Troubleshooting and fixing the test failure
-The purpose of this test is to validate the response matches the expected schema defined in schemas.py
-'''
+
 def test_pet_schema():
     test_endpoint = "/pets/1"
 
@@ -19,28 +16,32 @@ def test_pet_schema():
     # Validate the response schema against the defined schema in schemas.py
     validate(instance=response.json(), schema=schemas.pet)
 
-'''
-TODO: Finish this test by...
-1) Extending the parameterization to include all available statuses
-2) Validate the appropriate response code
-3) Validate the 'status' property in the response is equal to the expected status
-4) Validate the schema for each object in the response
-'''
-@pytest.mark.parametrize("status", [("available")])
+
+@pytest.mark.parametrize("status", [("pending"),("available")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
         "status": status
     }
-
+    expected_status = status
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+    assert response.status_code == 200
+    response_data = response.json()
+    for pet_info in response_data:
+        actual_status = pet_info.get('status')
+        assert actual_status == expected_status, f"Expected status: {expected_status}, Actual status: {actual_status}"
+   
+    # Validate the schema for each object in the response
+    for pet_info in response_data:
+         validate(instance=pet_info, schema=schemas.pet)
 
-'''
-TODO: Finish this test by...
-1) Testing and validating the appropriate 404 response for /pets/{pet_id}
-2) Parameterizing the test for any edge cases
-'''
-def test_get_by_id_404():
+
+
+@pytest.mark.parametrize("ids", [("8928627"),("*"),("-1000001")])
+def test_get_by_id_404(ids):
     # TODO...
+    test_endpoint = "/pets/" + ids
+    response = api_helpers.get_api_data(test_endpoint)
+    assert response.status_code == 404
+
     pass
