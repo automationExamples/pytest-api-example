45b882ab46229cae229fe93cc90e8e5b1097a462
tataatg <ntata@armstrongtransport.com>
2024-02-07 14:32:00 -0600

    Narendra Tata - Digital Technology Solutions

--- test_store.py
+++ test_store.py
@@ -1,6 +1,7 @@
 from jsonschema import validate
 import pytest
 import schemas
+import json
 import api_helpers
 from hamcrest import assert_that, contains_string, is_
 
@@ -12,5 +13,29 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+
+@pytest.fixture
+def create_test_order():
+    return {"id": 1, "pet_id": 101}
+
+def test_patch_order_by_id(create_test_order):
+    
+    id = create_test_order["id"]
+    test_endpoint = f"/store/orders/{id}"
+ 
+    existing_order = api_helpers.get_api_data(test_endpoint)
+    print(f"Existing Order Details: {existing_order}")
+    
+    payload = {"status": "placed"}
+    response = api_helpers.patch_api_data(test_endpoint, data=json.dumps(payload))
+
+    assert response.status_code == 200, f"Expected 200 OK, but got {response.status_code}"
+
+    updated_order = api_helpers.get_api_data(test_endpoint).json()
+
+    expected_message = "Order and pet status updated successfully"
+    assert response.json()["message"] == expected_message, f"Expected message: '{expected_message}'"
+
+    assert updated_order["status"] == payload["status"], "Order status was not updated successfully"
+
+    validate(instance=updated_order, schema=schemas.Order)
