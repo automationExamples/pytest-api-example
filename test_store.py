1eaca1c1d9c97d6100988f59987ca61c6179da81
hinturi <163608066+hinturi@users.noreply.github.com>
2024-03-15 22:52:45 -0400

    modified pet code
    Fixed the issue and updated code for others

--- test_store.py
+++ test_store.py
@@ -12,5 +12,31 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
+
+@pytest.fixture
+def create_payload():
+   return {"pet_id": 0}
+
+@pytest.fixture
+def update_payload():
+   return {"status": "available"}
+
+def test_patch_order_by_id(create_payload, update_payload):
+
+    # Create an order
+    test_endpoint = "/store/order"
+    response = api_helpers.post_api_data(test_endpoint, create_payload)
+
+    # Validate the order schema
+    assert response.status_code == 201
+    if response.status_code == 201:
+        validate(response.json(), schemas.order)
+
+    order_id = response.json().get('id')
+
+    test_endpoint = "/store/order/" + order_id
+    response = api_helpers.patch_api_data(test_endpoint, update_payload)
+
+    assert response.status_code == 200
+    assert response.json().get("message") == 'Order and pet status updated successfully'
     pass
